7c5a1bbcc5d7767435cadb52a17fdf92b320ad5a
Anonymous <anonymous@tor.com>
2019-10-23 00:04:43 +0800

    update

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -2,14 +2,14 @@
 <html>
     
     <head>
-        <link rel="stylesheet" href="./umi.css?v=0.1.3">
+        <link rel="stylesheet" href="./umi.css?v=0.1.4">
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width,initial-scale=1,maximum-scale=1,minimum-scale=1,user-scalable=no">
         <title>{{$title}}</title>
         <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Nunito+Sans:300,400,400i,600,700">
         <script>window.routerBase = "/";</script>
         <script>
-            window.v2panel = {
+            window.v2board = {
                 title: '{{$title}}'
             }
         </script>
@@ -17,7 +17,7 @@
     
     <body>
         <div id="root"></div>
-        <script src="./umi.js?v=0.1.3"></script>
+        <script src="./umi.js?v=0.1.4"></script>
     </body>
 
 </html>
