4604e6b0536d675bc878e439f2aaa7ee0da35d07
root <root@root.com>
2019-10-25 02:26:34 +0800

    update

--- app/Http/Controllers/Server/Controller.php
+++ app/Http/Controllers/Server/Controller.php
@@ -1,11 +1,12 @@
 <?php
 
 namespace App\Http\Controllers\Server;
+use Illuminate\Http\Request;
 use App\Http\Controllers\Controller as BaseController;
 
 class Controller extends BaseController
 {
-    public function __construct() {
+    public function __construct(Request $request) {
         $token = $request->input('token');
         if (empty($token)) {
             abort(500, 'token is null');
