f67d2fb2011b34fda50201ca5d7abc1809fdd053
root <root@root.com>
2019-10-27 20:31:38 +0800

    update

--- app/Http/Controllers/Admin/PlanController.php
+++ app/Http/Controllers/Admin/PlanController.php
@@ -32,6 +32,7 @@ class PlanController extends Controller
             $plan->content = str_replace(PHP_EOL, '', $plan->content);
         }
         $plan->show = $request->input('show');
+        $plan->renew = $request->input('renew');
         $plan->transfer_enable = $request->input('transfer_enable');
         $plan->group_id = $request->input('group_id');
         $plan->month_price = $request->input('month_price');
