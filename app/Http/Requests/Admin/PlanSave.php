02f8b6ca427d2677c9a050d894d79c07c34cac49
root <root@root.com>
2019-11-04 01:51:16 +0800

    update

--- app/Http/Requests/Admin/PlanSave.php
+++ app/Http/Requests/Admin/PlanSave.php
@@ -16,7 +16,11 @@ class PlanSave extends FormRequest
         return [
             'name' => 'required',
             'group_id' => 'required',
-            'transfer_enable' => 'required'
+            'transfer_enable' => 'required',
+            'month_price' => 'required|numeric',
+            'quarter_price' => 'required|numeric',
+            'half_year_price' => 'required|numeric',
+            'year_price' => 'required|numeric'
         ];
     }
     
@@ -25,7 +29,11 @@ class PlanSave extends FormRequest
         return [
             'name.required' => '套餐名称不能为空',
             'group_id.required' => '权限组不能为空',
-            'transfer_enable.required' => '流量不能为空'
+            'transfer_enable.required' => '流量不能为空',
+            'month_price.required' => '月付金额不能为空',
+            'quarter_price.required' => '季付金额不能为空',
+            'half_year_price.required' => '半年付金额不能为空',
+            'year_price.required' => '年付金额不能为空'
         ];
     }
 }
