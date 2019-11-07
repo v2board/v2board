1d79bea0f50109e07a998458593fa1d6a05f9c6b
root <root@root.com>
2019-11-07 21:24:47 +0800

    update

--- app/Http/Controllers/ClientController.php
+++ app/Http/Controllers/ClientController.php
@@ -38,7 +38,7 @@ class ClientController extends Controller
     private function quantumultX ($user, $server) {
       $uri = '';
       foreach($server as $item) {
-        $uri .= "vmess=".$item->host.":".$item->port.", method=none, password=".$user->v2ray_uuid.", over-tls=".($item->tls?'true':'false').", certificate=0, fast-open=false, udp-relay=false, tag=".$item->name;
+        $uri .= "vmess=".$item->host.":".$item->port.", method=none, password=".$user->v2ray_uuid.", fast-open=false, udp-relay=false, tag=".$item->name;
         if ($item->network == 'ws') {
           $uri .= ', obfs=ws';
           if ($item->settings) {
