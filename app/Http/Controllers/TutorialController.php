715e0175cf3c474517d821db5ca97fb1dc496635
root <root@root.com>
2019-11-23 01:44:02 +0800

    update

--- app/Http/Controllers/TutorialController.php
+++ app/Http/Controllers/TutorialController.php
@@ -20,13 +20,13 @@ class TutorialController extends Controller
         if ($user->expired_at < time()) {
             return response([
                 'data' => false
-            ])
+            ]);
         }
         return response([
             'data' => [
                 'apple_id' => config('v2board.apple_id'),
                 'apple_id_password' => config('v2board.apple_id_password')
             ]
-        ])
+        ]);
     }
 }
