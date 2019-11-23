15af0ae2dec9f23ab18723f3b6d20355de834ee2
root <root@root.com>
2019-11-23 23:35:54 +0800

    update

---
+++ app/Console/Commands/AppUpdate.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use App\Models\Order;
+use App\Models\User;
+
+class AppUpdate extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:update';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = '从git强制覆盖更新';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+    }
+    
+}
