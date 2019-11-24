eeac29acebe20ed7f3928ddcc7550c265877b07b
root <root@root.com>
2019-11-25 00:22:52 +0800

    update

--- app/Http/Controllers/Server/DeepbworkController.php
+++ app/Http/Controllers/Server/DeepbworkController.php
@@ -17,10 +17,11 @@ class DeepbworkController extends Controller
     public function user (Request $request) {
         $nodeId = $request->input('node_id');
         $server = Server::find($nodeId);
-        if ($server) {
-            $server->last_check_at = time();
-            $server->save();
+        if (!$server) {
+            abort(500, 'fail');
         }
+        $server->last_check_at = time();
+        $server->save();
         $users = User::whereIn('group_id', json_decode($server->group_id))
             ->select([
                 'id',
