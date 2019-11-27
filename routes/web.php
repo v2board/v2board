47bb393f2bc25ee9a0f689d72546632c261bea85
root <root@root.com>
2019-11-27 20:21:09 +0800

    update

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,6 @@ Route::get('/', function () {
     return view('app', [
         'title' => config('v2board.app_name', 'V2Board'),
         'theme' => config('v2board.app_theme', 1),
-        'verison' => '0.1.8'
+        'verison' => '0.1.9'
     ]);
 });
