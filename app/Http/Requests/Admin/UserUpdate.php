5dcc3568f8823219101d8d2e6e9431997e7f6225
root <root@root.com>
2019-11-27 20:07:31 +0800

    update

--- app/Http/Requests/Admin/UserUpdate.php
+++ app/Http/Requests/Admin/UserUpdate.php
@@ -18,7 +18,9 @@ class UserUpdate extends FormRequest
             'transfer_enable' => 'numeric',
             'expired_at' => 'integer',
             'banned' => 'required|in:0,1',
-            'is_admin' => 'required|in:0,1'
+            'is_admin' => 'required|in:0,1',
+            'plan_id' => 'integer',
+            'commission_rate' => 'integer|min:0|max:100'
         ];
     }
     
@@ -32,7 +34,11 @@ class UserUpdate extends FormRequest
             'banned.required' => '是否封禁不能为空',
             'banned.in' => '是否封禁格式不正确',
             'is_admin.required' => '是否管理员不能为空',
-            'is_admin.in' => '是否管理员格式不正确'
+            'is_admin.in' => '是否管理员格式不正确',
+            'plan_id.integer' => '订阅计划格式不正确',
+            'commission_rate.integer' => '推荐返利比例格式不正确',
+            'commission_rate.min' => '推荐返利比例最小为0',
+            'commission_rate.max' => '推荐返利比例最大为100'
         ];
     }
 }
