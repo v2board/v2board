9161c382e95593cf6a78d48055258dcf40786022
root <root@root.com>
2019-12-16 14:08:25 +0800

    update

--- app/Http/Controllers/Admin/TicketController.php
+++ app/Http/Controllers/Admin/TicketController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers\Admin;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
-use App\Models\User;
 use App\Models\Ticket;
 use App\Models\TicketMessage;
 use Illuminate\Support\Facades\DB;
