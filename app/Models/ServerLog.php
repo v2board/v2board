34bb04d7554509cb2b9cfcae890b7642a66c4c46
root <root@root.com>
2019-12-17 17:37:15 +0800

    update

--- app/Models/ServerLog.php
+++ app/Models/ServerLog.php
@@ -2,7 +2,6 @@
 
 namespace App\Models;
 
-use App\Events\ServerCreatedEvent;
 use Illuminate\Database\Eloquent\Model;
 
 class ServerLog extends Model
@@ -10,6 +9,5 @@ class ServerLog extends Model
     protected $table = 'v2_server_log';
     protected $dateFormat = 'U';
     protected $dispatchesEvents = [
-        'created' => ServerCreatedEvent::class
     ];
 }
--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -13,9 +13,6 @@ class EventServiceProvider extends ServiceProvider
      * @var array
      */
     protected $listen = [
-        'App\Events\ServerCreatedEvent' => [
-            'App\Listeners\ServerCreatedListener',
-        ],
     ];
 
     /**
