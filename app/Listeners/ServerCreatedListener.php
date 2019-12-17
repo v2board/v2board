ad01888662e90a41a771ea924670aaa4ba6f33f8
root <root@root.com>
2019-12-17 17:13:57 +0800

    update

--- app/Listeners/ServerCreatedListener.php
+++ app/Listeners/ServerCreatedListener.php
@@ -25,6 +25,6 @@ class ServerCreatedListener
      * @return void
      */
     public function handle(ServerCreatedEvent $event) {
-        info('listener', $event->serverLog);
+        info('listener', json_encode($event->serverLog));
     }
 }
