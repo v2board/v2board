c2d090be5a3b9ccb0e01d9b764edfafd464ed79f
root <root@root.com>
2019-12-23 01:10:24 +0800

    update

--- app/Console/Commands/ResetServerLog.php
+++ app/Console/Commands/ResetServerLog.php
@@ -6,7 +6,7 @@ use Illuminate\Console\Command;
 use Illuminate\Support\Facades\DB;
 use App\Models\ServerLog;
 
-class ResetTraffic extends Command
+class ResetServerLog extends Command
 {
     /**
      * The name and signature of the console command.
