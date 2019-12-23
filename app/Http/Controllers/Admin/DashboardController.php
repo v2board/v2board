56d0305bf040dcf122ef951d07d5779cf9c2a269
root <root@root.com>
2019-12-23 16:37:47 +0800

    update

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -19,8 +19,6 @@ class DashboardController extends Controller
             'data' => [
                 'month_income' => Redis::get('month_income'),
                 'month_register_total' => Redis::get('month_register_total'),
-                'server_offline_total' => Server::where('last_check_at', '<', time() - 300)
-                    ->count(),
                 'ticket_pendding_total' => Ticket::where('status', 0)
                     ->count(),
                 'commission_pendding_total' => Order::where('commission_status', 0)
