988f088a589ef83e899f85f45a7bed80e1813955
root <root@root.com>
2019-12-27 17:36:02 +0800

    update

--- app/Http/Requests/Admin/PlanSave.php
+++ app/Http/Requests/Admin/PlanSave.php
@@ -17,10 +17,10 @@ class PlanSave extends FormRequest
             'name' => 'required',
             'group_id' => 'required',
             'transfer_enable' => 'required',
-            'month_price' => 'required|numeric',
-            'quarter_price' => 'required|numeric',
-            'half_year_price' => 'required|numeric',
-            'year_price' => 'required|numeric'
+            'month_price' => 'numeric',
+            'quarter_price' => 'numeric',
+            'half_year_price' => 'numeric',
+            'year_price' => 'numeric'
         ];
     }
     
@@ -30,10 +30,10 @@ class PlanSave extends FormRequest
             'name.required' => '套餐名称不能为空',
             'group_id.required' => '权限组不能为空',
             'transfer_enable.required' => '流量不能为空',
-            'month_price.required' => '月付金额不能为空',
-            'quarter_price.required' => '季付金额不能为空',
-            'half_year_price.required' => '半年付金额不能为空',
-            'year_price.required' => '年付金额不能为空'
+            'month_price.numeric' => '月付金额格式有误',
+            'quarter_price.numeric' => '季付金额格式有误',
+            'half_year_price.numeric' => '半年付金额格式有误',
+            'year_price.numeric' => '年付金额格式有误'
         ];
     }
 }
