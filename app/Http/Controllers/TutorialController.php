9e55cb2f5de9a8107c04c5bffc616be230ce8f98
root <root@root.com>
2020-01-02 21:52:20 +0800

    update

--- app/Http/Controllers/TutorialController.php
+++ app/Http/Controllers/TutorialController.php
@@ -36,5 +36,19 @@ class TutorialController extends Controller
 
     public function fetch (Request $request) {
         $tutorial = Tutorial::select(['title', 'description', 'icon'])->get();
+        $response = [
+            'data' => [
+                'tutorials' => $tutorial,
+                'subscribe_url' => config('v2board.subscribe_url', config('v2board.app_url', env('APP_URL'))) . '/api/v1/client/subscribe?token=' . $user['token']
+            ]
+        ];
+        $user = User::find($request->session()->get('id'));
+        if ($user->expired_at > time()) {
+            $response['data']['apple_id'] = [
+                'apple_id' => config('v2board.apple_id'),
+                'apple_id_password' => config('v2board.apple_id_password')
+            ];
+        }
+        return response($response);
     }
 }
