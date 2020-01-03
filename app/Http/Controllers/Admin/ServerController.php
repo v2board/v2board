2d58744de4acfffafcc39a516b10c2506222c1c5
root <root@root.com>
2020-01-03 12:38:25 +0800

    update

--- app/Http/Controllers/Admin/ServerController.php
+++ app/Http/Controllers/Admin/ServerController.php
@@ -22,7 +22,7 @@ class ServerController extends Controller
             }
             $server[$i]['group_id'] = json_decode($server[$i]['group_id']);
             if ($server[$i]['parent_id']) {
-                Redis::get('server_last_check_at_' . $server[$i]['parent_id']);
+                $server[$i]['last_check_at'] = Redis::get('server_last_check_at_' . $server[$i]['parent_id']);
             } else {
                 $server[$i]['last_check_at'] = Redis::get('server_last_check_at_' . $server[$i]['id']);
             }
