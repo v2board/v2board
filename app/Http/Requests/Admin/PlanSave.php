b9cee3664163a266840cbceb27b4697f14a86fff
root <root@root.com>
2020-01-03 19:12:05 +0800

    update

--- app/Http/Requests/Admin/PlanSave.php
+++ app/Http/Requests/Admin/PlanSave.php
@@ -17,10 +17,10 @@ class PlanSave extends FormRequest
             'name' => 'required',
             'group_id' => 'required',
             'transfer_enable' => 'required',
-            'month_price' => 'nullable｜numeric',
-            'quarter_price' => 'nullable｜numeric',
-            'half_year_price' => 'nullable｜numeric',
-            'year_price' => 'nullable｜numeric'
+            'month_price' => 'nullable|numeric',
+            'quarter_price' => 'nullable|numeric',
+            'half_year_price' => 'nullable|numeric',
+            'year_price' => 'nullable|numeric'
         ];
     }
     
