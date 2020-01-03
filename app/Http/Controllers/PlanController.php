a524fb6f76f5679d2ae7be2f965b4dd4af8df5c6
root <root@root.com>
2020-01-03 21:04:44 +0800

    update

--- app/Http/Controllers/PlanController.php
+++ app/Http/Controllers/PlanController.php
@@ -9,13 +9,19 @@ use App\Models\Plan;
 class PlanController extends Controller
 {
     public function fetch (Request $request) {
-        if (empty($request->input('plan_id'))) {
-            abort(500, '参数错误');
+        if ($request->input('id')) {
+            $plan = Plan::where('id', $request->input('id'))
+                ->where('show', 1)
+                ->first();
+            if (!$plan) {
+                abort(500, '该订阅不存在');
+            }
+            return response([
+                'data' => $plan
+            ]);
         }
-        $plan = Plan::find($request->input('plan_id'));
-        if (!$plan) {
-            abort(500, '该订阅不存在');
-        }
+        $plan = Plan::where('show', 1)
+            ->get();
         return response([
             'data' => $plan
         ]);
