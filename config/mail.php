fc3c4b3b7329384b7dacdefe64f03f7040808d2e
Tokumeikoi <tokumeikoi@protonmail.com>
2020-01-31 17:54:41 +0800

    update

--- config/mail.php
+++ config/mail.php
@@ -16,7 +16,7 @@ return [
     |
     */
 
-    'driver' => env('MAIL_DRIVER', 'smtp'),
+    'driver' => config('v2board.mail_driver', env('MAIL_DRIVER', 'smtp')),
 
     /*
     |--------------------------------------------------------------------------
@@ -29,7 +29,7 @@ return [
     |
     */
 
-    'host' => env('MAIL_HOST', 'smtp.mailgun.org'),
+    'host' => config('v2board.mail_host', env('MAIL_HOST', 'smtp.mailgun.org')),
 
     /*
     |--------------------------------------------------------------------------
@@ -42,7 +42,7 @@ return [
     |
     */
 
-    'port' => env('MAIL_PORT', 587),
+    'port' => config('v2board.mail_port', env('MAIL_PORT', 587)),
 
     /*
     |--------------------------------------------------------------------------
@@ -56,8 +56,8 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'hello@example.com'),
-        'name' => env('MAIL_FROM_NAME', 'Example'),
+        'address' => config('v2board.mail_from_address', env('MAIL_FROM_ADDRESS', 'hello@example.com')),
+        'name' => config('v2board.mail_from_name', env('MAIL_FROM_NAME', 'Example')),
     ],
 
     /*
@@ -71,7 +71,7 @@ return [
     |
     */
 
-    'encryption' => env('MAIL_ENCRYPTION', 'tls'),
+    'encryption' => config('v2board.mail_encryption', env('MAIL_ENCRYPTION', 'tls')),
 
     /*
     |--------------------------------------------------------------------------
@@ -84,9 +84,9 @@ return [
     |
     */
 
-    'username' => env('MAIL_USERNAME'),
+    'username' => config('v2board.mail_username', env('MAIL_USERNAME')),
 
-    'password' => env('MAIL_PASSWORD'),
+    'password' => config('v2board.mail_password', env('MAIL_PASSWORD')),
 
     /*
     |--------------------------------------------------------------------------
