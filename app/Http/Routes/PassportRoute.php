d744fba6839296fc488a01c812f356eeee85ce24
Tokumeikoi <tokumeikoi@protonmail.com>
2020-02-01 20:57:15 +0800

    split router

--- app/Http/Routes/PassportRoute.php
+++ app/Http/Routes/PassportRoute.php
@@ -11,14 +11,14 @@ class PassportRoute
             'prefix' => 'passport'
         ], function ($router) {
             // Auth
-            $router->post('/auth/register', 'Passport\\AuthController@register');
-            $router->post('/auth/login', 'Passport\\AuthController@login');
-            $router->get('/auth/token2Login', 'Passport\\AuthController@token2Login');
-            $router->get('/auth/check', 'Passport\\AuthController@check');
-            $router->post('/auth/forget', 'Passport\\AuthController@index');
+            $router->post('auth/register', 'Passport\\AuthController@register');
+            $router->post('auth/login', 'Passport\\AuthController@login');
+            $router->get('auth/token2Login', 'Passport\\AuthController@token2Login');
+            $router->get('auth/check', 'Passport\\AuthController@check');
+            $router->post('auth/forget', 'Passport\\AuthController@index');
             // Comm
-            $router->get('/comm/config', 'Passport\\CommController@config');
-            $router->post('/comm/sendEmailVerify', 'Passport\\CommController@sendEmailVerify');
+            $router->get('comm/config', 'Passport\\CommController@config');
+            $router->post('comm/sendEmailVerify', 'Passport\\CommController@sendEmailVerify');
         });
     }
 }
