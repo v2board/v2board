1f471545d49c98b3fa1c88980b39b8bf61f732b4
Tokumeikoi <tokumeikoi@protonmail.com>
2020-02-08 16:38:03 +0800

    add safe mode

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Http\Request;
 */
 
 Route::get('/', function (Request $request) {
-    if (config('v2board.app_url')) {
+    if (config('v2board.app_url') && config('v2board.safe_mode_enable', 0)) {
         if ($request->server('HTTP_HOST') !== parse_url(config('v2board.app_url'))['host']) {
             abort(403);
         }
