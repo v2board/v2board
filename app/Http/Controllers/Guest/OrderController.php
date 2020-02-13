03ddd3640636a390ff2bcff63b1d9fe6a1140210
PYPY <>
2020-02-14 04:41:06 +0800

    Update OrderController.php

--- app/Http/Controllers/Guest/OrderController.php
+++ app/Http/Controllers/Guest/OrderController.php
@@ -71,7 +71,7 @@ class OrderController extends Controller
                     'amount' => $source['amount'],
                     'currency' => $source['currency'],
                     'source' => $source['id'],
-                    'description' => $source['metadata']['invoice_id']
+                    'description' => config('v2board.app_name')."#".$source['metadata']['invoice_id']
                 ]);
                 if ($charge['status'] == 'succeeded') {
                     $trade_no = Cache::get($source['id']);
