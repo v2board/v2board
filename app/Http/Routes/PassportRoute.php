540b331f169803bd58e290022108177ca091a9b9
Tokumeikoi <tokumeikoi@protonmail.com>
2020-02-13 13:44:58 +0800

    fix forget

--- app/Http/Routes/PassportRoute.php
+++ app/Http/Routes/PassportRoute.php
@@ -15,7 +15,7 @@ class PassportRoute
             $router->post('/auth/login', 'Passport\\AuthController@login');
             $router->get ('/auth/token2Login', 'Passport\\AuthController@token2Login');
             $router->get ('/auth/check', 'Passport\\AuthController@check');
-            $router->post('/auth/forget', 'Passport\\AuthController@index');
+            $router->post('/auth/forget', 'Passport\\AuthController@forget');
             // Comm
             $router->get ('/comm/config', 'Passport\\CommController@config');
             $router->post('/comm/sendEmailVerify', 'Passport\\CommController@sendEmailVerify');
