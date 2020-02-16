bff92180b0fec1a53913521bcd16a0d31bea78be
Tokumeikoi <tokumeikoi@protonmail.com>
2020-02-17 03:37:04 +0800

    update theme

--- public/assets/admin/env.example.js
+++ public/assets/admin/env.example.js
@@ -1,12 +1,14 @@
 window.v2board = {
   // 站点标题
   title: 'V2Board',
-  // 站点描述
-  description: 'V2Board is best',
   // API
   host: '',
   // 主题
-  theme: '1',
+  theme: {
+    sidebar: 'light',
+    header: 'dark',
+    color: 'default'
+  },
   // 背景
   background_url: ''
 }
