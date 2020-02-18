61b31c19252462fab9848db42e63e8f41f6b46f8
Tokumeikoi <tokumeikoi@protonmail.com>
2020-02-18 22:48:00 +0800

    update tutorial

--- app/Http/Requests/Admin/TutorialSave.php
+++ app/Http/Requests/Admin/TutorialSave.php
@@ -9,7 +9,7 @@ class TutorialSave extends FormRequest
     CONST RULES = [
         'title' => 'required',
         // 1:windows 2:macos 3:ios 4:android 5:linux 6:router
-        'category' => 'required|in:1,2,3,4,5,6',
+        'category_id' => 'required|in:1,2,3,4,5,6',
         'description' => 'required',
         'icon' => 'required'
     ];
@@ -27,8 +27,8 @@ class TutorialSave extends FormRequest
     {
         return [
             'title.required' => '标题不能为空',
-            'category.required' => '分类不能为空',
-            'category.in' => '分类格式不正确',
+            'category_id.required' => '分类不能为空',
+            'category_id.in' => '分类格式不正确',
             'description.required' => '描述不能为空',
             'icon.required' => '图标不能为空'
         ];
