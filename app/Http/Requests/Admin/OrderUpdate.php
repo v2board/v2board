018e4aa81005d157ecd28d8d89efbeb57dbac903
Tokumeikoi <tokumeikoi@protonmail.com>
2020-02-23 02:05:23 +0800

    fix commission status update

--- app/Http/Requests/Admin/OrderUpdate.php
+++ app/Http/Requests/Admin/OrderUpdate.php
@@ -15,7 +15,7 @@ class OrderUpdate extends FormRequest
     {
         return [
             'status' => 'in:0,1,2,3',
-            'commission_status' => 'in:0,1'
+            'commission_status' => 'in:0,1,2'
         ];
     }
 
@@ -23,7 +23,7 @@ class OrderUpdate extends FormRequest
     {
         return [
             'status.in' => '销售状态格式不正确',
-            'commission_status.in' => '续费状态格式不正确'
+            'commission_status.in' => '佣金状态格式不正确'
         ];
     }
 }
