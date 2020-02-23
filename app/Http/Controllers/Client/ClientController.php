05186d43c279f4544139b80457cee402a886161f
DesperadoJ <38433236+DesperadoJ@users.noreply.github.com>
2020-02-23 22:07:11 +0800

    Refine code
    Use "stripos" instead of "strpos"

--- app/Http/Controllers/Client/ClientController.php
+++ app/Http/Controllers/Client/ClientController.php
@@ -33,10 +33,7 @@ class ClientController extends Controller
             if (strpos($_SERVER['HTTP_USER_AGENT'], 'Quantumult') !== false) {
                 die($this->quantumult($user, $server));
             }
-            if (strpos(strtolower($_SERVER['HTTP_USER_AGENT']), 'clashforandroid') !== false) {
-                die($this->clash($user, $server));
-            }
-            if (strpos(strtolower($_SERVER['HTTP_USER_AGENT']), 'clash') !== false) {
+            if (stripos($_SERVER['HTTP_USER_AGENT'], 'clash') !== false) {
                 die($this->clash($user, $server));
             }
         }
