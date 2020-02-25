2171ef59d78293334a0ac74d557aca461623cc89
Tokumeikoi <tokumeikoi@protonmail.com>
2020-02-25 09:56:06 +0800

    fix tutorial

--- app/Http/Requests/Admin/TutorialSave.php
+++ app/Http/Requests/Admin/TutorialSave.php
@@ -10,7 +10,8 @@ class TutorialSave extends FormRequest
         'title' => 'required',
         // 1:windows 2:macos 3:ios 4:android 5:linux 6:router
         'category_id' => 'required|in:1,2,3,4,5,6',
-        'icon' => 'required'
+        'icon' => 'required',
+        'steps' => 'required'
     ];
     /**
      * Get the validation rules that apply to the request.
@@ -28,7 +29,8 @@ class TutorialSave extends FormRequest
             'title.required' => '标题不能为空',
             'category_id.required' => '分类不能为空',
             'category_id.in' => '分类格式不正确',
-            'icon.required' => '图标不能为空'
+            'icon.required' => '图标不能为空',
+            'steps.required' => '教程步骤不能为空'
         ];
     }
 }
