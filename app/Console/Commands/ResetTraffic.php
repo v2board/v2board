10e4de39deed12caf97ecba77484dcf1915f034e
Tokumeikoi <tokumeikoi@protonmail.com>
2020-02-28 01:21:14 +0800

    add onetime plan

--- app/Console/Commands/ResetTraffic.php
+++ app/Console/Commands/ResetTraffic.php
@@ -4,6 +4,8 @@ namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\DB;
+use App\Models\Plan;
+use App\Models\User;
 
 class ResetTraffic extends Command
 {
