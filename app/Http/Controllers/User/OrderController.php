56cabbdc00336bcbcc3067bd4c4241a2fbf47dcd
Tokumeikoi <tokumeikoi@protonmail.com>
2020-03-15 21:26:56 +0800

    update

--- app/Http/Controllers/User/OrderController.php
+++ app/Http/Controllers/User/OrderController.php
@@ -206,8 +206,9 @@ class OrderController extends Controller
         }
         // invite process
         if ($user->invite_user_id && $order->total_amount > 0) {
-            if (!((int)config('v2board.commission_first_time', 1) && Order::where('user_id', $user->id)->where('status', 3)->first())) {
-                $order->invite_user_id = $user->invite_user_id;
+            $order->invite_user_id = $user->invite_user_id;
+            $commissionFirstTime = (int)config('v2board.commission_first_time', 1);
+            if ($commissionFirstTime || !($commissionFirstTime && Order::where('user_id', $user->id)->where('status', 3)->first())) {
                 $inviter = User::find($user->invite_user_id);
                 if ($inviter && $inviter->commission_rate) {
                     $order->commission_balance = $order->total_amount * ($inviter->commission_rate / 100);
