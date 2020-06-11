f24b38a4ba3967d9f0118e3c03c38c1462c5b4ab
Tokumeikoi <tokumeikoi@protonmail.com>
2020-06-11 21:50:54 +0800

    update trojan server

--- app/Http/Controllers/Admin/Server/TrojanController.php
+++ app/Http/Controllers/Admin/Server/TrojanController.php
@@ -22,7 +22,7 @@ class TrojanController extends Controller
                 $server[$i]['tags'] = json_decode($server[$i]['tags']);
             }
             $server[$i]['group_id'] = json_decode($server[$i]['group_id']);
-            $server[$i]['online'] = Cache::get(CacheKey::get('SERVER_ONLINE_USER', $server[$i]['id']));
+            $server[$i]['online'] = Cache::get(CacheKey::get('SERVER_TROJAN_ONLINE_USER', $server[$i]['id']));
         }
         return response([
             'data' => $server
