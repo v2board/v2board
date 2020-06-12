2fc77a38f281e4b54e7fa4c39d69e05a91658007
Tokumeikoi <tokumeikoi@protonmail.com>
2020-06-12 17:16:21 +0800

    fix trojan shadowrocket remarks

--- app/Utils/Helper.php
+++ app/Utils/Helper.php
@@ -59,7 +59,7 @@ class Helper
 
     public static function buildTrojanLink(ServerTrojan $server, User $user)
     {
-        $server->name = urlencode($server->name);
+        $server->name = rawurlencode($server->name);
         $uri = "trojan://{$user->uuid}@{$server->host}:{$server->port}#{$server->name}";
         $uri .= "\r\n";
         return $uri;
