ebce59a0d14e26b892f3ee0c557a414bdcd79e77
Tokumeikoi <tokumeikoi@protonmail.com>
2020-11-02 22:22:18 +0800

    fix: stat

--- app/Http/Controllers/Admin/StatController.php
+++ app/Http/Controllers/Admin/StatController.php
@@ -29,16 +29,16 @@ class StatController extends Controller
                     ->count(),
                 'commission_pendding_total' => Order::where('commission_status', 0)
                     ->where('invite_user_id', '!=', NULL)
-                    ->where('status', 3)
+                    ->where('status', [3, 4])
                     ->where('commission_balance', '>', 0)
                     ->count(),
                 'day_income' => Order::where('created_at', '>=', strtotime(date('Y-m-d')))
                     ->where('created_at', '<', time())
-                    ->where('status', 3)
+                    ->where('status', [3, 4])
                     ->sum('total_amount'),
                 'last_month_income' => Order::where('created_at', '>=', strtotime('-1 month', strtotime(date('Y-m-1'))))
                     ->where('created_at', '<', strtotime(date('Y-m-1')))
-                    ->where('status', 3)
+                    ->where('status', [3, 4])
                     ->sum('total_amount')
             ]
         ]);
