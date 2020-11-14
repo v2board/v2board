2fc7c8c07cc5a77913c4f0b982c2d25b5a3c60fb
Tokumeikoi <tokumeikoi@protonmail.com>
2020-11-14 17:27:54 +0800

    clear: files

--- app/Http/Controllers/Admin/Server/ShadowsocksController.php
+++ app/Http/Controllers/Admin/Server/ShadowsocksController.php
@@ -15,26 +15,6 @@ use Illuminate\Support\Facades\DB;
 
 class ShadowsocksController extends Controller
 {
-    public function fetch(Request $request)
-    {
-        $server = ServerShadowsocks::orderBy('sort', 'ASC')->get();
-        for ($i = 0; $i < count($server); $i++) {
-            if (!empty($server[$i]['tags'])) {
-                $server[$i]['tags'] = json_decode($server[$i]['tags']);
-            }
-            $server[$i]['group_id'] = json_decode($server[$i]['group_id']);
-            $server[$i]['online'] = Cache::get(CacheKey::get('SERVER_SHADOWSOCKS_ONLINE_USER', $server[$i]['parent_id'] ? $server[$i]['parent_id'] : $server[$i]['id']));
-            if ($server[$i]['parent_id']) {
-                $server[$i]['last_check_at'] = Cache::get(CacheKey::get('SERVER_SHADOWSOCKS_LAST_CHECK_AT', $server[$i]['parent_id']));
-            } else {
-                $server[$i]['last_check_at'] = Cache::get(CacheKey::get('SERVER_SHADOWSOCKS_LAST_CHECK_AT', $server[$i]['id']));
-            }
-        }
-        return response([
-            'data' => $server
-        ]);
-    }
-
     public function save(ServerShadowsocksSave $request)
     {
         $params = $request->validated();
@@ -113,21 +93,6 @@ class ShadowsocksController extends Controller
             abort(500, '复制失败');
         }
 
-        return response([
-            'data' => true
-        ]);
-    }
-
-    public function sort(ServerShadowsocksSort $request)
-    {
-        DB::beginTransaction();
-        foreach ($request->input('server_ids') as $k => $v) {
-            if (!ServerShadowsocks::find($v)->update(['sort' => $k + 1])) {
-                DB::rollBack();
-                abort(500, '保存失败');
-            }
-        }
-        DB::commit();
         return response([
             'data' => true
         ]);
