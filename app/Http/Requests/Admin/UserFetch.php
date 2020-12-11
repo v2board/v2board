db3e2abbbf48fd3e0ea19848347bc06a9eb779e9
tokumeikoi <tokumeikoi@protonmail.com>
2020-12-11 20:40:27 +0800

    update: user filter

--- app/Http/Requests/Admin/UserFetch.php
+++ app/Http/Requests/Admin/UserFetch.php
@@ -14,7 +14,7 @@ class UserFetch extends FormRequest
     public function rules()
     {
         return [
-            'filter.*.key' => 'required|in:id,email,transfer_enable,d,expired_at,uuid,token,invite_by_email,invite_user_id',
+            'filter.*.key' => 'required|in:id,email,transfer_enable,d,expired_at,uuid,token,invite_by_email,invite_user_id,plan_id',
             'filter.*.condition' => 'required|in:>,<,=,>=,<=,模糊',
             'filter.*.value' => 'required'
         ];
