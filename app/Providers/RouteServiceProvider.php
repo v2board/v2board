d18d90371fde8a7f3e74f5f9dbbe6179782a8bea
Julian Liu <wlootlxt123@gmail.com>
2021-01-24 07:19:29 +0800

    Strip client/server middleware, eliminate session overhead

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -40,7 +40,7 @@ class RouteServiceProvider extends ServiceProvider
         $this->mapApiRoutes();
         $this->mapWebRoutes();
 
-        //
+        $this->mapStaticRoutes();
     }
 
     /**
