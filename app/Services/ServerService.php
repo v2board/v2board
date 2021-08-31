91418caf04cd933034845dfa6faa6de858a11ae4
tokumeikoi <tokumeikoi@protonmail.com>
2021-09-01 03:32:15 +0900

    update: log record

--- app/Services/ServerService.php
+++ app/Services/ServerService.php
@@ -260,6 +260,7 @@ class ServerService
 
     public function log(int $userId, int $serverId, int $u, int $d, float $rate, string $method)
     {
+        if (($u + $d) < 10240) return true;
         $timestamp = strtotime(date('Y-m-d H:0'));
         $serverLog = ServerLog::where('log_at', '>=', $timestamp)
             ->where('log_at', '<', $timestamp + 3600)
