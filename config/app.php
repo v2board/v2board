35e11a68162fb7a792de3dd8662d4ac77688cf55
tokumeikoi <tokumeikoi@protonmail.com>
2021-09-01 01:56:16 +0900

    update: add horizon

--- config/app.php
+++ config/app.php
@@ -173,6 +173,7 @@ return [
         App\Providers\AuthServiceProvider::class,
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
+        App\Providers\HorizonServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
     ],
