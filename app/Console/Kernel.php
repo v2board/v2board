8085c2ba6a92781d9f448e934e2b697a8dad3141
tokumeikoi <tokumeikoi@protonmail.com>
2021-10-01 17:37:49 +0900

    update: schedule

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -31,7 +31,7 @@ class Kernel extends ConsoleKernel
         $schedule->command('check:commission')->everyMinute();
         // reset
         $schedule->command('reset:traffic')->daily();
-        $schedule->command('reset:serverLog')->quarterly();
+        $schedule->command('reset:serverLog')->quarterly()->at('0:15');
         // send
         $schedule->command('send:remindMail')->dailyAt('11:30');
         // horizon metrics
