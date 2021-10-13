f380bd3c0e5e0681956219e2d135a4177c33501f
tokumeikoi <tokumeikoi@protonmail.com>
2021-10-13 21:04:02 +0800

    update: install

--- app/Console/Commands/V2boardInstall.php
+++ app/Console/Commands/V2boardInstall.php
@@ -48,7 +48,7 @@ class V2boardInstall extends Command
             $this->info("  \ V /  / __/| |_) | (_) | (_| | | | (_| | ");
             $this->info("   \_/  |_____|____/ \___/ \__,_|_|  \__,_| ");
             if (\File::exists(base_path() . '/.env')) {
-                abort(500, 'V2board 已安装，如需重新安装请删除目录下.lock文件');
+                abort(500, 'V2board 已安装，如需重新安装请删除目录下.env文件');
             }
 
             if (!copy(base_path() . '/.env.example', base_path() . '/.env')) {
