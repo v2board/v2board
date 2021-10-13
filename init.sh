8e7f0bbc44345bb012c958c8837f3404303a15fd
tokumeikoi <tokumeikoi@protonmail.com>
2021-10-13 21:00:21 +0800

    update: shell

--- init.sh
+++ init.sh
@@ -1,4 +1,4 @@
 rm -rf composer.phar
-curl https://github.com/composer/composer/releases/latest/download/composer.phar > composer.phar
+wget https://github.com/composer/composer/releases/latest/download/composer.phar -O composer.phar
 php composer.phar install -vvv
 php artisan v2board:install
