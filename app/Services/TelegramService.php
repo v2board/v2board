ce8b5dde28563a01a87d378f768c89dcf24ba61f
tokumeikoi <tokumeikoi@protonmail.com>
2021-12-23 14:25:05 +0800

    update: fix telegram markdown

--- app/Services/TelegramService.php
+++ app/Services/TelegramService.php
@@ -17,7 +17,7 @@ class TelegramService {
     public function sendMessage(int $chatId, string $text, string $parseMode = '')
     {
         if ($parseMode === 'markdown') {
-            $text = Markdown::parse($text);
+            $text = str_replace('_', '\_', $text);
         }
         $this->request('sendMessage', [
             'chat_id' => $chatId,
