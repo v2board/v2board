e29261a87f261e46e98208520f47fd158bb34ccf
tokumeikoi <tokumeikoi@protonmail.com>
2021-12-26 19:35:24 +0800

    update: get reset day

--- app/Http/Controllers/User/UserController.php
+++ app/Http/Controllers/User/UserController.php
@@ -189,6 +189,8 @@ class UserController extends Controller
     private function getResetDay(User $user)
     {
         if ($user->expired_at <= time() || $user->expired_at === NULL) return null;
+        // if reset method is not reset
+        if (isset($user->plan->reset_traffic_method) && $user->plan->reset_traffic_method === 2) return null;
         $day = date('d', $user->expired_at);
         $today = date('d');
         $lastDay = date('d', strtotime('last day of +0 months'));
