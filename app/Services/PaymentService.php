82584cb18dd49655a5e0bc40ce6fbdcd3de8bcdf
tokumeikoi <tokumeikoi@protonmail.com>
2022-04-17 02:17:20 +0800

    update: payment

--- app/Services/PaymentService.php
+++ app/Services/PaymentService.php
@@ -39,7 +39,7 @@ class PaymentService
     public function pay($order)
     {
         // custom notify domain name
-        $notifyUrl = url("/api/v1/guest/payment/notify/{$this->method}/{$this->config['uuid']}");
+        $notifyUrl = config('v2board.app_url', env('APP_URL')) . "/api/v1/guest/payment/notify/{$this->method}/{$this->config['uuid']}";
         if ($this->config['notify_domain']) {
             $parseUrl = parse_url($notifyUrl);
             $notifyUrl = $this->config['notify_domain'] . $parseUrl['path'];
