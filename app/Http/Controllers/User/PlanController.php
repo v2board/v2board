472a692b3c8b9704ab706ccc75b29025ddf473e8
tokumeikoi <tokumeikoi@protonmail.com>
2022-04-19 04:10:02 +0800

    update: logic optimization

--- app/Http/Controllers/User/PlanController.php
+++ app/Http/Controllers/User/PlanController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\User;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
 use Illuminate\Http\Request;
 use App\Models\Plan;
 
@@ -10,10 +11,13 @@ class PlanController extends Controller
 {
     public function fetch(Request $request)
     {
+        $user = User::find($request->session()->get('id'));
         if ($request->input('id')) {
-            $plan = Plan::where('id', $request->input('id'))
-                ->first();
+            $plan = Plan::where('id', $request->input('id'))->first();
             if (!$plan) {
+                abort(500, __('Subscription plan does not exist'));
+            }
+            if ((!$plan->show && !$plan->renew) || (!$plan->show && $user->plan_id !== $plan->id)) {
                 abort(500, __('Subscription plan does not exist'));
             }
             return response([
