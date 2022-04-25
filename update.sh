c8e2d54d2be34bab1d9c60a2db58ed808f6076a8
tokumeikoi <tokumeikoi@protonmail.com>
2022-04-26 01:07:03 +0800

    update: support laravel 8

--- update.sh
+++ update.sh
@@ -2,6 +2,5 @@ git fetch --all && git reset --hard origin/master && git pull origin master
 rm -rf composer.lock composer.phar
 wget https://github.com/composer/composer/releases/latest/download/composer.phar -O composer.phar
 php composer.phar update -vvv
-php artisan horizon:publish
 php artisan v2board:update
 php artisan config:cache
