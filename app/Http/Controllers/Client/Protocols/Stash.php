25cae4343071d67f7d2e720d72fd595afbdb6363
coldice945 <coldice945@hotmail.com>
2022-05-19 23:01:11 +0800

    修复如果网站名称为中文的时候，CFW and Stash 不显示文件名或显示文件名乱码的问题

--- app/Http/Controllers/Client/Protocols/Stash.php
+++ app/Http/Controllers/Client/Protocols/Stash.php
@@ -24,6 +24,7 @@ class Stash
         header("subscription-userinfo: upload={$user['u']}; download={$user['d']}; total={$user['transfer_enable']}; expire={$user['expired_at']}");
         header('profile-update-interval: 24');
         header("content-disposition: filename={$appName}");
+        header("content-disposition: filename*=UTF-8''".rawurlencode($appName));
         // 暂时使用clash配置文件，后续根据Stash更新情况更新
         $defaultConfig = base_path() . '/resources/rules/default.clash.yaml';
         $customConfig = base_path() . '/resources/rules/custom.clash.yaml';
