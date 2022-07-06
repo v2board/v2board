2823f1bd47664b5487024c0e232ea6259a32b4b4
tokumeikoi <tokumeikoi@protonmail.com>
2022-07-07 04:04:47 +0800

    update: reset log

---
+++ app/Console/Commands/ResetLog.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\Plan;
+use App\Models\StatUser;
+use App\Utils\Helper;
+use Illuminate\Console\Command;
+use App\Models\User;
+use Illuminate\Support\Facades\DB;
+
+class ResetLog extends Command
+{
+    protected $builder;
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'reset:log';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = '清空日志';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        StatUser::where('record_at', '<', strtotime('-2 month', time()))
+            ->delete();
+    }
+}
--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -35,6 +35,7 @@ class Kernel extends ConsoleKernel
         $schedule->command('check:ticket')->everyMinute();
         // reset
         $schedule->command('reset:traffic')->daily();
+        $schedule->command('reset:log')->daily();
         // send
         $schedule->command('send:remindMail')->dailyAt('11:30');
         // horizon metrics
