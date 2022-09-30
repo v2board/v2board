b6ce8314cd1b15c7c7c71f87e8cbff0e3f4aa353
tokumeikoi <tokumeikoi@protonmail.com>
2022-10-01 02:32:54 +0800

    update: fix close ticket rules

--- app/Console/Commands/CheckTicket.php
+++ app/Console/Commands/CheckTicket.php
@@ -41,6 +41,7 @@ class CheckTicket extends Command
         ini_set('memory_limit', -1);
         $tickets = Ticket::where('status', 0)
             ->where('updated_at', '<=', time() - 24 * 3600)
+            ->where('reply_status', 0)
             ->get();
         foreach ($tickets as $ticket) {
             if ($ticket->user_id === $ticket->last_reply_user_id) continue;
