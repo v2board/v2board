b3a03ae44761153ea13a137241cbb569430973f6
hunter6698 <eng.homan@gmail.com>
2022-12-06 20:32:21 +0330

    Translation of files inside the app/payment folder

--- app/Payments/Coinbase.php
+++ app/Payments/Coinbase.php
@@ -11,7 +11,7 @@ class Coinbase {
     {
         return [
             'coinbase_url' => [
-                'label' => '接口地址',
+                'label' => 'Interface Address',
                 'description' => '',
                 'type' => 'input',
             ],
@@ -31,8 +31,8 @@ class Coinbase {
     public function pay($order) {
 
         $params = [
-            'name' => '订阅套餐',
-            'description' => '订单号 ' . $order['trade_no'],
+            'name' => 'Subscription Package',
+            'description' => 'Order Number ' . $order['trade_no'],
             'pricing_type' => 'fixed_price',
             'local_price' => [
                 'amount' => sprintf('%.2f', $order['total_amount'] / 100),
