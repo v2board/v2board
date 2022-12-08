8a03cf7a2b84105dabfc9377463b81f54b700741
hunter6698 <eng.homan@gmail.com>
2022-12-08 14:15:29 +0330

    Return to previous state

--- public/theme/v2board/dashboard.blade.php
+++ public/theme/v2board/dashboard.blade.php
@@ -32,13 +32,23 @@
             version: '{{$version}}',
             background_url: '{{$theme_config['background_url']}}',
             description: '{{$description}}',
-            i18n: [             
-                'en-US'          
+            i18n: [
+                'zh-CN',
+                'en-US',
+                'ja-JP',
+                'vi-VN',
+                'ko-KR',
+                'zh-TW'
             ],
             logo: '{{$logo}}'
         }
     </script>
+    <script src="/theme/{{$theme}}/assets/i18n/zh-CN.js?v={{$version}}"></script>
+    <script src="/theme/{{$theme}}/assets/i18n/zh-TW.js?v={{$version}}"></script>
     <script src="/theme/{{$theme}}/assets/i18n/en-US.js?v={{$version}}"></script>
+    <script src="/theme/{{$theme}}/assets/i18n/ja-JP.js?v={{$version}}"></script>
+    <script src="/theme/{{$theme}}/assets/i18n/vi-VN.js?v={{$version}}"></script>
+    <script src="/theme/{{$theme}}/assets/i18n/ko-KR.js?v={{$version}}"></script>
 </head>
 
 <body>
