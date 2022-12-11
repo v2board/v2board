a1b4568e483b7d0e74f650a3159e28f07cb0eeb2
matador7495 <id4acc2014@gmail.com>
2022-12-11 21:11:18 +0330

    Translating the text of emails into Persian language

---
+++ resources/views/mail/persian-default/remindExpire.blade.php
@@ -0,0 +1,42 @@
+<div style="background: #eee">
+    <table width="600" border="0" align="center" cellpadding="0" cellspacing="0">
+        <tbody>
+        <tr>
+            <td>
+                <div style="background:#fff">
+                    <table width="100%" border="0" cellspacing="0" cellpadding="0">
+                        <thead>
+                        <tr>
+                            <td valign="middle" style="padding-left:30px;background-color:#415A94;color:#fff;padding:20px 40px;font-size: 21px;"> {{$name}} </td>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        <tr style="padding:40px 40px 0 40px;display:table-cell">
+                            <td style="font-size:24px;line-height:1.5;color:#000;margin-top:40px">هشدار اشتراک شما رو به اتمام است</td>
+                        </tr>
+                        <tr>
+                            <td style="font-size:14px;color:#333;padding:24px 40px 0 40px">
+                                سلام کاربر عزیز!
+                                <br />
+                                <br />
+                                اشتراک شما 24 ساعت دیگر منقضی می شود. لطفا در اسرع وقت اشتراک خود را تمدید کنید تا در استفاده شما اختلال ایجاد نشود. اگر قبلا اشتراک خود را تمدید کرده اید، لطفاً این ایمیل را نادیده بگیرید.
+                            </td>
+                        </tr>
+                        <tr style="padding:40px;display:table-cell">
+                        </tr>
+                        </tbody>
+                    </table>
+                </div>
+                <div>
+                    <table width="100%" border="0" cellspacing="0" cellpadding="0">
+                        <tbody>
+                        <tr>
+                            <td style="padding:20px 40px;font-size:12px;color:#999;line-height:20px;background:#f7f7f7"><a href="{{$url}}" style="font-size:14px;color:#929292"> باز گشت به سایت {{$name}} </a></td>
+                        </tr>
+                        </tbody>
+                    </table>
+                </div></td>
+        </tr>
+        </tbody>
+    </table>
+</div>
