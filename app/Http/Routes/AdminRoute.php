3f7ecb23dfda6d4bde31d69fdafbaa3ad6632ea5
tokumeikoi <tokumeikoi@protonmail.com>
2022-12-15 11:29:54 +0800

    update: default secure path

--- app/Http/Routes/AdminRoute.php
+++ app/Http/Routes/AdminRoute.php
@@ -8,7 +8,7 @@ class AdminRoute
     public function map(Registrar $router)
     {
         $router->group([
-            'prefix' => config('v2board.secure_path', config('v2board.frontend_admin_path', 'admin')),
+            'prefix' => config('v2board.secure_path', config('v2board.frontend_admin_path', hash('crc32b', config('app.key')))),
             'middleware' => 'admin'
         ], function ($router) {
             // Config
