e72450c7d4f9fa4b2f8d39947e8de49898f7bebf
matador7495 <id4acc2014@gmail.com>
2022-12-27 20:33:34 +0330

    Translation of Telegram bot messages in English

--- app/Plugins/Telegram/Commands/Bind.php
+++ app/Plugins/Telegram/Commands/Bind.php
@@ -7,32 +7,32 @@ use App\Plugins\Telegram\Telegram;
 
 class Bind extends Telegram {
     public $command = '/bind';
-    public $description = '将Telegram账号绑定到网站';
+    public $description = 'Bind your Telegram account to the site';
 
     public function handle($message, $match = []) {
         if (!$message->is_private) return;
         if (!isset($message->args[0])) {
-            abort(500, '参数有误，请携带订阅地址发送');
+            abort(500, 'There is an error in the parameters, please send with your subscription address');
         }
         $subscribeUrl = $message->args[0];
         $subscribeUrl = parse_url($subscribeUrl);
         parse_str($subscribeUrl['query'], $query);
         $token = $query['token'];
         if (!$token) {
-            abort(500, '订阅地址无效');
+            abort(500, 'The subscription address is invalid');
         }
         $user = User::where('token', $token)->first();
         if (!$user) {
-            abort(500, '用户不存在');
+            abort(500, 'The user does not exist');
         }
         if ($user->telegram_id) {
-            abort(500, '该账号已经绑定了Telegram账号');
+            abort(500, 'The account is already bind to a Telegram account');
         }
         $user->telegram_id = $message->chat_id;
         if (!$user->save()) {
-            abort(500, '设置失败');
+            abort(500, 'Setup failure');
         }
         $telegramService = $this->telegramService;
-        $telegramService->sendMessage($message->chat_id, '绑定成功');
+        $telegramService->sendMessage($message->chat_id, 'Binding successfully');
     }
 }
