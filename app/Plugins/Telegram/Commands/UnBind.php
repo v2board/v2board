9964315bebac48de987452dbb749f8ce12185988
matador7495 <id4acc2014@gmail.com>
2022-12-27 21:35:20 +0330

    Translation of Telegram bot files in English

--- app/Plugins/Telegram/Commands/UnBind.php
+++ app/Plugins/Telegram/Commands/UnBind.php
@@ -7,20 +7,20 @@ use App\Plugins\Telegram\Telegram;
 
 class UnBind extends Telegram {
     public $command = '/unbind';
-    public $description = '将Telegram账号从网站解绑';
+    public $description = 'Unbundle your Telegram account from the site';
 
     public function handle($message, $match = []) {
         if (!$message->is_private) return;
         $user = User::where('telegram_id', $message->chat_id)->first();
         $telegramService = $this->telegramService;
         if (!$user) {
-            $telegramService->sendMessage($message->chat_id, '没有查询到您的用户信息，请先绑定账号', 'markdown');
+            $telegramService->sendMessage($message->chat_id, 'Your user information is not available, please bind your account first', 'markdown');
             return;
         }
         $user->telegram_id = NULL;
         if (!$user->save()) {
-            abort(500, '解绑失败');
+            abort(500, 'Unbinding failed');
         }
-        $telegramService->sendMessage($message->chat_id, '解绑成功', 'markdown');
+        $telegramService->sendMessage($message->chat_id, 'Unbinding successful', 'markdown');
     }
 }
