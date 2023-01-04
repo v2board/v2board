5943ec5e3a31c0897bf5d69aae8aaf81e7059bc9
MinghaoChow <54492656@qq.com>
2023-01-04 10:47:07 +0800

    支持V2ray VLESS / XTLS 配置下发

--- app/Http/Controllers/Client/Protocols/Shadowrocket.php
+++ app/Http/Controllers/Client/Protocols/Shadowrocket.php
@@ -33,7 +33,7 @@ class Shadowrocket
                 $uri .= self::buildShadowsocks($user['uuid'], $item);
             }
             if ($item['type'] === 'v2ray') {
-                $uri .= self::buildVmess($user['uuid'], $item);
+                $uri .= self::buildV2ray($user['uuid'], $item);
             }
             if ($item['type'] === 'trojan') {
                 $uri .= self::buildTrojan($user['uuid'], $item);
@@ -64,18 +64,22 @@ class Shadowrocket
         return "ss://{$str}@{$server['host']}:{$server['port']}#{$name}\r\n";
     }
 
-    public static function buildVmess($uuid, $server)
+    public static function buildV2ray($uuid, $server)
     {
         $userinfo = base64_encode('auto:' . $uuid . '@' . $server['host'] . ':' . $server['port']);
         $config = [
             'tfo' => 1,
-            'remark' => $server['name'],
-            'alterId' => 0
+            'remark' => $server['name']
         ];
+        if ($server['type'] === '0') {
+            $config['alterId'] = 0;
+        }
         if ($server['tls']) {
             $config['tls'] = 1;
             if ($server['tlsSettings']) {
                 $tlsSettings = $server['tlsSettings'];
+                if (isset($server['type'] === '1' && $tlsSettings['xtls']) && !empty($tlsSettings['xtls']))
+                    $config['xtls'] = (int)$tlsSettings['xtls'];
                 if (isset($tlsSettings['allowInsecure']) && !empty($tlsSettings['allowInsecure']))
                     $config['allowInsecure'] = (int)$tlsSettings['allowInsecure'];
                 if (isset($tlsSettings['serverName']) && !empty($tlsSettings['serverName']))
@@ -106,7 +110,11 @@ class Shadowrocket
             }
         }
         $query = http_build_query($config, '', '&', PHP_QUERY_RFC3986);
-        $uri = "vmess://{$userinfo}?{$query}";
+        if ($server['type'] === '1')
+            $uri = "vless://";
+        else
+            $uri = "vmess://";
+        $uri = $uri . "{$userinfo}?{$query}";
         $uri .= "\r\n";
         return $uri;
     }
