3b1159187fe5d80d302bb1757c1f8189aaf3ef2a
v2board <org@v2board.com>
2023-05-03 18:24:30 +0800

    update: user statistical

--- app/Http/Controllers/User/StatController.php
+++ app/Http/Controllers/User/StatController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\User;
 
 use App\Http\Controllers\Controller;
 use App\Models\StatUser;
+use App\Services\StatisticalService;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
@@ -11,7 +12,10 @@ class StatController extends Controller
 {
     public function getTrafficLog(Request $request)
     {
-        $builder = StatUser::select([
+        $statService = new StatisticalService();
+        $statService->setStartAt(strtotime(date('Y-m-d')));
+        $statService->setUserStats();
+        $stats = StatUser::select([
             'u',
             'd',
             'record_at',
@@ -20,9 +24,17 @@ class StatController extends Controller
         ])
             ->where('user_id', $request->user['id'])
             ->where('record_at', '>=', strtotime(date('Y-m-1')))
-            ->orderBy('record_at', 'DESC');
+            ->orderBy('record_at', 'DESC')
+            ->get()
+            ->toArray();
+
+        $stats = array_merge($stats, $statService->getStatUserByUserID($request->user['id']));
+        usort($stats, function ($a, $b) {
+            return $b['record_at'] - $a['record_at'];
+        });
+
         return response([
-            'data' => $builder->get()
+            'data' => $stats
         ]);
     }
 }
