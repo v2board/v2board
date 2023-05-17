2f045055626d9670c5f214fc957719f91e063863
v2board <org@v2board.com>
2023-05-17 11:30:18 +0800

    update: clash enhanced mode force fake-ip

--- app/Http/Controllers/Client/Protocols/Clash.php
+++ app/Http/Controllers/Client/Protocols/Clash.php
@@ -34,7 +34,6 @@ class Clash
         } else {
             $config = Yaml::parseFile($defaultConfig);
         }
-        $this->patch($config);
         $proxy = [];
         $proxies = [];
 
@@ -178,12 +177,5 @@ class Clash
     private function isRegex($exp)
     {
         return @preg_match($exp, null) !== false;
-    }
-
-    private function patch(&$config)
-    {
-        // fix clash x dns mode
-        preg_match('#(ClashX)[/ ]([0-9.]*)#', $_SERVER['HTTP_USER_AGENT'], $matches);
-        if (isset($matches[2]) && $matches[2] < '1.96.2') $config['dns']['enhanced-mode'] = 'redir-host';
     }
 }
